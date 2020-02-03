04f92add74c3474588762a3e423da2079fa0cc78
jorge <jorge@192.168.1.75>
2020-02-03 22:35:52 +0100

    Actualización vistas

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -2,6 +2,16 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     
     <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-57636987-32"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-57636987-32');
+</script>
+
         
         <link href="{{ asset('css/web.css') }}" rel="stylesheet">
         <meta charset="utf-8">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,16 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     
-    <head>
+    <head><!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-57636987-32"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-57636987-32');
+</script>
+
         
         <link href="{{ asset('css/web.css') }}" rel="stylesheet">
         <meta charset="utf-8">
