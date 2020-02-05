cb71ac364a81f613d5007b9ba279526c093341c7
jorge <jorge@192.168.1.75>
2020-02-05 22:50:06 +0100

    Actualización vistas

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -33,6 +33,9 @@
                 <div class="row">
                     @yield('content')
                 </div>
+                <div class="row">
+                    <p>“Chollomix.com participa en el Programa de Afiliados de Amazon EU, un programa de publicidad para afiliados diseñado para ofrecer a sitios web un modo de obtener comisiones por publicidad, publicitando e incluyendo enlaces a Amazon.co.uk/ Amazon.de/ de.buyvip.com/Amazon.fr/Amazon.it/it.buyvip.com/ Amazon.es/es.buyvip.com.”</p>
+                </div>
             </div>
         
     </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,6 +32,9 @@
                 <div class="row">
                     @include('web.products.product-list')
                 </div>
+                <div class="row">
+                    <p>“Chollomix.com participa en el Programa de Afiliados de Amazon EU, un programa de publicidad para afiliados diseñado para ofrecer a sitios web un modo de obtener comisiones por publicidad, publicitando e incluyendo enlaces a Amazon.co.uk/ Amazon.de/ de.buyvip.com/Amazon.fr/Amazon.it/it.buyvip.com/ Amazon.es/es.buyvip.com.”</p>
+                </div>
             </div>
         
     </body>
