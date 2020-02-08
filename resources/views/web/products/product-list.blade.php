1dac5b3861b9ad74b5b89ef98969077503c00f41
jorge <jorge@192.168.1.75>
2020-02-08 15:29:25 +0100

    Actualización crawler

--- resources/views/web/products/product-list.blade.php
+++ resources/views/web/products/product-list.blade.php
@@ -24,7 +24,9 @@
                     <a alt="Comprar {{substr($product->title,0,120)}}" title=" Comprar {{ substr($product->title,0,120)}}" class="col-md-12 text-center" target="_blank" href="https://www.amazon.es/gp/product/{{$product->sku}}/ref=as_li_ss_tl?pf_rd_p=17a988a6-15ad-46df-8d9f-8365d36240ce&pf_rd_r=6XBGH18821MAEHEYZMCH&tag=lalupadesherlockcom-21&">
                         <button class="btn btn-success btn-lg">Comprar</button>
                     </a>
+                        @if (!empty($price))
                         <p class="text-center">Precio verificado el:<br> {{ date('d-m-Y H:i', strtotime($price->updated_at)) }}</p>
+                        @endif
                 
             </div>
         </div>
