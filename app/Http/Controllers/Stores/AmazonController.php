d9a8b92caf955054e9370c30baca6af870723bcf
jorge <jorge@192.168.1.75>
2020-02-08 14:41:10 +0100

    Actualización crawler

--- app/Http/Controllers/Stores/AmazonController.php
+++ app/Http/Controllers/Stores/AmazonController.php
@@ -208,8 +208,9 @@ class AmazonController extends Controller
                 ),
         ));
         curl_exec($ch);
+        $html = file_get_contents($web);
         
-        dd($ch);
+        dd($html);
     }
 }
 
