120b786fea0efd75d74b55fa9425f22f3fb44cbf
jorge <jorge@192.168.1.75>
2020-02-08 14:43:36 +0100

    Actualización crawler

--- app/Http/Controllers/Stores/AmazonController.php
+++ app/Http/Controllers/Stores/AmazonController.php
@@ -199,12 +199,12 @@ class AmazonController extends Controller
         $ch=curl_init($web);
         curl_setopt_array($ch,array(
                 CURLOPT_USERAGENT=>'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0',
-                CURLOPT_ENCODING=>'gzip, deflate',
                 CURLOPT_HTTPHEADER=>array(
-                        'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                        'Accept-Language: es-ES,en;q=0.5',
-                        'Connection: keep-alive',
-                        'Upgrade-Insecure-Requests: 1',
+                                        'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                'Accept-Language: en-US,en;q=0.5',
+                'Accept-Encoding: gzip, deflate',
+                'Connection: keep-alive',
+                'Upgrade-Insecure-Requests: 1',
                 ),
         ));
         curl_exec($ch);
